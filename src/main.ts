62c2699c038afd20b1f7cf126f4477e01b6e240a
windlliu <windlliu@tencent.com>
2022-10-17 20:24:47 +0800

    chore: 优化算法

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ import { saveToCache } from './utils/cache.js'
 
 const usedFiles = new Set<string>() // 在使用中文件
 const routes = getRoute() // 路由映射
-const map = new Map<string, Set<string>>() // 文件间的加载关系
+const map = new Map<string, Set<string>>() // 文件间的加载关系, 树形
 const appear = new Set<string>()  // 满足查询条件的文件
 
 function getDependanceList(entry: string) {
@@ -34,21 +34,23 @@ function getDependanceList(entry: string) {
   const ast = babel.parseSync(entry_str, config.AST_CONFIG as babel.TransformOptions)
   const dirPath = path2.dirname(entry)
 
+  let set = map.get(entry)
+  if (!set) {
+    set = new Set()
+    map.set(entry, set)
+  }
+
   let visitor: babel.Visitor = {
     ImportDeclaration(path) {
       const { value } = path.node.source
       if (checkFileSuffix(value)) { // 校验文件后缀
         const absPath = getAbsFilePath(dirPath, value)
-        if (!map.has(absPath)) {
-          map.set(absPath, new Set(entry))
-        } else {
-          const mset = map.get(absPath)
-          mset.add(entry)
-        }
-
-        if (absPath && !usedFiles.has(absPath)) {
-          usedFiles.add(absPath)
-          getDependanceList(absPath)
+        if (absPath) {
+          set.add(absPath)
+          if (!usedFiles.has(absPath)) {
+            usedFiles.add(absPath)
+            getDependanceList(absPath)
+          }
         }
       }
     },
@@ -56,14 +58,20 @@ function getDependanceList(entry: string) {
       if (t.isIdentifier(path.node, { name: 'areaCodeArr' }) || t.isIdentifier(path.node, { name: 'CommunitySelect' })) {
         appear.add(entry)
       }
+      // if (t.isIdentifier(path.node, { name: 'getAddrSuggestions' })) {
+      //   appear.add(entry)
+      // }
     }
   }
   if (entry === config.ROUTE_PATH) { // 路由处理
     const routeVisitor = getVisitor(
       (absPath) => {
-        if (absPath && !usedFiles.has(absPath)) {
-          usedFiles.add(absPath)
-          getDependanceList(absPath)
+        if (absPath) {
+          set.add(absPath)
+          if (!usedFiles.has(absPath)) {
+            usedFiles.add(absPath)
+            getDependanceList(absPath)
+          }
         }
       }
     )
@@ -74,32 +82,44 @@ function getDependanceList(entry: string) {
 
 getDependanceList(config.ENTRY_PATH)
 
-// saveToCache(`routes.json`, routes)
-// saveToCache('usedFiles.json', [...usedFiles])
-saveToCache('appear.json', [...appear])
+// saveToCache(`routes.json`, [...routes.keys()])
+// saveToCache('appear.json', [...appear])
+// saveToCache('map.json', [...map.keys()].reduce((res, k) => {
+//   const v = map.get(k)
+//   if (v && v.size) {
+//     res[k] =  [...v]
+//   }
+//   return res
+// }, {}))
 
 const res = new Set();
 (() => {
-  [...appear].map((appearFile) => {
-    if (routes.has(appearFile)) {
-      res.add(routes.get(appearFile))
-    } else {
-      loop(appearFile)
+  function checkFile(file: string) {
+    let used = new Set()
+    let stack = [file]
+    while (stack.length) {
+      const tmp = stack.shift()
+      if (used.has(tmp)) { // 排除循环执行
+        continue
+      }
+      used.add(tmp)
+      if (appear.has(tmp)) {
+        return true
+      }
+      const fileSet = map.get(tmp)
+      if (fileSet && fileSet.size) {
+        stack = [...new Set([...stack, ...fileSet])]
+      }
+    }
+    return false
+  }
+
+  routes.forEach((route, file) => {
+    const checked = checkFile(file)
+    if (checked) {
+      res.add(route)
     }
   })
-
-  function loop(file: string) {
-    if (map.has(file)) {
-      const pp = map.get(file)
-      pp.forEach((item) => {
-        if (routes.has(item)) {
-          res.add(routes.get(item))
-        } else {
-          loop(item)
-        }
-      })
-    }
-  }
 })()
 
-console.log([...res])
+saveToCache('res.json', [...res])
