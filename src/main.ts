dce082722200b30eaf69f825e5ddd9b0a343f526
windlliu <windlliu@tencent.com>
2023-12-08 14:36:28 +0800

    feat: 修改入口文件

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 import { Cli, Builtins } from 'clipanion';
-import StashCommand from './stash/index.js';
-import TrashCommand from './trash/index.js';
-import DevViteCommand from './dev-vite/index.js';
+import StashCommand from './stash/command.js';
+import TrashCommand from './trash/command.js';
+import DevViteCommand from './dev-vite/command.js';
+import FindCommand from './find/command.js';
 
 const [node, app, ...args] = process.argv;
 
@@ -13,6 +14,7 @@ const cli = new Cli({
   binaryVersion: `0.0.1`,
 });
 cli.register(Builtins.HelpCommand);
+cli.register(FindCommand);
 cli.register(StashCommand);
 cli.register(TrashCommand);
 cli.register(DevViteCommand);
